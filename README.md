815a02c96bf62eaecdabadf01288a19536ef1677
Owen <owenshen24@gmail.com>
2019-10-12 00:23:27 -0700

    Updated README and added default sizing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # edit-svg
-Edit an SVG online. Update the XML and see changes in real-time.
+Edit an SVG online by directly editing the XML!
+The one thing I felt like [SVGO](https://jakearchibald.github.io/svgomg/) lacks is an easy way to manipulate the source XML of an SVG file. The other tools I found online didn't support hot-reloading
+
+![Screenshot of the user interface.](images/screenshot.png)
+
+Open Source Projects this project uses:
+- [CodeMirror](https://codemirror.net/) for the XML highlighting.
+- [svg-pan-zoom](https://github.com/ariutta/svg-pan-zoom) for SVG panning/zooming.
--- scripts/ui.js
+++ scripts/ui.js
@@ -4,6 +4,8 @@ let code = CodeMirror.fromTextArea(document.getElementById("textarea"), {
   lineNumbers: true
 });
 
+let default_svg = "<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 210 297\">\r\n  <defs\/>\r\n  <rect width=\"78.6\" height=\"78.6\" x=\"51\" y=\"83\" fill=\"#dc3c3c\" rx=\"0\" ry=\"0\"\/>\r\n  <rect width=\"65.2\" height=\"65.2\" x=\"57\" y=\"90\" fill=\"#ffa619\" rx=\"0\" ry=\"0\"\/>\r\n  <rect width=\"50.3\" height=\"50.3\" x=\"65\" y=\"98\" fill=\"#f5e700\" rx=\"0\" ry=\"0\"\/>\r\n  <rect width=\"38.2\" height=\"38.2\" x=\"71\" y=\"104\" fill=\"#96dd67\" rx=\"0\" ry=\"0\"\/>\r\n  <rect width=\"24.5\" height=\"24.5\" x=\"78\" y=\"111\" fill=\"#57a2d9\" rx=\"0\" ry=\"0\"\/>\r\n  <rect width=\"14.3\" height=\"14.3\" x=\"83\" y=\"116\" fill=\"#9244cf\" rx=\"0\" ry=\"0\"\/>\r\n  <rect width=\"6.2\" height=\"6.2\" x=\"-26.5\" y=\"148\" fill=\"#fff\" rx=\"0\" ry=\"0\" transform=\"rotate(-45)\"\/>\r\n<\/svg>\r\n";
+
 let parser = new DOMParser();
 function makeSVG(s, id) {
   let svg = parser.parseFromString(s, "image/svg+xml").documentElement;
@@ -12,7 +14,7 @@ function makeSVG(s, id) {
 $(document).ready( function() {
 
   let file_name = "";
-
+  code.setValue(default_svg);
   // 
   $(".render").click(function() {
     let svg = makeSVG(code.getValue(), 'svg');
@@ -24,7 +26,7 @@ $(document).ready( function() {
         svg.removeAttribute(a);
       }
       svg.setAttribute('width', '100%');
-      svg.setAttribute('height', '83vh');
+      svg.setAttribute('height', $(".svg-holder").width());
       svg.setAttribute('preserveAspectRatio', 'xMinYMin meet')
     }
 
