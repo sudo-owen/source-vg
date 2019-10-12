b857d2df204ada20415a159439efc9bd12a1b178
Owen <owenshen24@gmail.com>
2019-10-11 17:55:20 -0700

    Added Upload and Download

--- scripts/ui.js
+++ scripts/ui.js
@@ -9,12 +9,34 @@ function makeSVG(s, id) {
   return(svg);
 }
 $(document).ready( function() {
+
+  let file_name = "";
+
+  // 
   $(".render").click(function() {
     let svg = makeSVG(code.getValue(), 'svg');
-    svg.removeAttribute('viewBox');
-    svg.setAttribute('viewBox', '0 0 500 500');
-    svg.setAttribute('preserveAspectRatio', 'xMinYMin meet')
+    // svg.removeAttribute('viewBox');
+    // svg.setAttribute('viewBox', '0 0 500 500');
+    // svg.setAttribute('preserveAspectRatio', 'xMinYMin meet')
     $(".svg-holder").find(':first-child').remove();
     $(".svg-holder").append(svg);
+  });
+
+  $(".inputfile").change(function(e) {
+    file_name = e.target.files[0].name;
+    e.target.files[0].text().then(function(src) {
+      code.setValue(src);
+    });
+  });
+  
+  $(".download").click(function() {
+    let src = code.getValue();
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(src));
+    anchor.setAttribute('download', file_name);
+    anchor.style.display = 'none';
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
   });
 });
